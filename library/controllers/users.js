094114cc06b2a3bb2299fed24d188878b8d92aa2
Abhijit kumar <abhijitkumar360@gmail.com>
2021-05-25 10:16:04 +0530

    new file

--- library/controllers/users.js
+++ library/controllers/users.js
@@ -2,6 +2,11 @@ import  {User} from '../models/user.js'
 let users=[]
 
 export const getUsers=(req,res)=>{
+    console.log("Hi")
+    console.log(req.query)
+    if(req.query.age){
+        getUsersByAge(req,res)
+    }else{
     console.log("in function call getUser()... i think");
     // res.send(users)
     User.find()
@@ -16,7 +21,7 @@ export const getUsers=(req,res)=>{
                     
                 }
             ) 
-}
+} }
 
 export const getUserByID=(req,res)=>{
     console.log("in function call getUserByID()... i think");
@@ -45,6 +50,52 @@ export const createUser=(req,res)=>{
         city: req.body.city, 
     })
     user.save()
+    .then(
+        (result)=>{
+            res.send(result)
+        }
+    )
+    .catch(
+        (err)=>{
+            console.log(err)
+        }
+    )
+}
+export const deleteUserById= (req,res)=>{
+    console.log("In function call deleteUserById()..I think this '/users' endpoint got hit.")
+    // res.send(users)
+    User.findByIdAndDelete(req.params.id)
+        .then(
+            (result)=>{
+                res.send(result)
+            }
+        )
+        .catch(
+            (err)=>{
+                console.log(err)
+            }
+        )
+}
+export const updateUserById = (req,res) => {    
+    User.findByIdAndUpdate(req.params.id,{
+        age: req.body.age,
+        city: req.body.city 
+    }) .then(
+        (result)=>{
+            res.send(result)
+        }
+    )
+    .catch(
+        (err)=>{
+            console.log(err)
+        }
+    )
+}
+
+const getUsersByAge = (req,res) => {
+    User.aggregate(
+        [{$sort:{age:1}}]
+    )
     .then(
         (result)=>{
             res.send(result)
--- library/routers/users.js
+++ library/routers/users.js
@@ -1,13 +1,13 @@
 import express from 'express';
- import { createUser, getUserByID} from '../../library/controllers/users.js';
-import {getUsers} from '../controllers/users.js'
+import { createUser, getUserByID} from '../../library/controllers/users.js';
+import {getUsers, deleteUserById} from '../controllers/users.js'
 // import { getUserByID } from '../../library1/controllers/users';
 const router =express.Router();
 
 router.get("/",getUsers)
 router.get("/:id",getUserByID)
- router.post("/",createUser)
-// router.delete("/id",deleteUserById)
-// router.patch("/id",deleteUserById)
+router.post("/",createUser)
+router.delete("/id",deleteUserById)
+router.patch("/id",deleteUserById)
 
 export default router
